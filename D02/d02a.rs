091da3ebc18813ba09b1656fdc116c67785b5a11
mjiam <mjiam@student.codam.nl>
2020-12-03 18:59:58 +0100

    Day 2 completed

---
+++ D02/d02a.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+	let mut file = File::open("input").unwrap();
+	let mut content = String::new();
+	file.read_to_string(&mut content).unwrap();
+
+	let mut valid_pass = 0;
+	for line in content.lines() {
+		let line2 = &line.replace(":", " ");
+		let chunks: Vec<_> = line2.split_whitespace().collect();
+		// chunks[0] = range, chunks[1] = char, chunks[2] = password
+		let bounds: Vec<_> = chunks[0].split("-").collect();
+		// bounds[0] = lower limit, bounds[1] = upper limit
+		let count = chunks[2].matches(chunks[1]).count();
+		if bounds[0].parse::<usize>().unwrap() <= count {
+			if count <= bounds[1].parse::<usize>().unwrap() {
+				valid_pass += 1;
+			}
+		}
+	}
+	println!("Number of valid passwords: {}", valid_pass);
+}
---
+++ D02/d02b.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+	let mut file = File::open("input").unwrap();
+	let mut content = String::new();
+	file.read_to_string(&mut content).unwrap();
+
+	let mut valid_pass = 0;
+	for line in content.lines() {
+		let line2 = &line.replace(":", " ").replace("-", " ");
+		let chunks: Vec<_> = line2.split_whitespace().collect();
+		// chunks[0] = first pos, chunks[1] = second pos, chunks[2] = char, chunks[3] = password
+		let pos_1 = chunks[0].parse::<usize>().unwrap();
+		let pos_2 = chunks[1].parse::<usize>().unwrap();
+		let c1 = chunks[3].chars().nth(pos_1 - 1);
+		let c2 = chunks[3].chars().nth(pos_2 - 1);
+		let letter = chunks[2].parse::<char>().unwrap();
+		if (c1.unwrap() == letter) ^ (c2.unwrap() == letter) {
+			valid_pass += 1;
+		}
+	}
+	println!("Number of valid passwords: {}", valid_pass);
+}
