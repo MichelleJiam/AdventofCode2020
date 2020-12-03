cbe356284a1e3b2e4e3940a09b189d78a1acbd58
mjiam <mjiam@student.codam.nl>
2020-12-03 16:21:08 +0100

    First day finished

---
+++ D01/d01a.rs
@@ -0,0 +1,22 @@
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+	let mut file = File::open("input").unwrap();
+	let mut numbers = String::new();
+	file.read_to_string(&mut numbers).unwrap();
+	let mut vector: Vec<i32> = vec![];
+
+	for line in numbers.lines() {
+		vector.push(line.parse::<i32>().unwrap());
+	}
+	for i in vector.iter() {
+		for j in vector.iter() {
+			if i + j == 2020 {
+				println!("{} * {} = {}", i, j, i * j);
+				return();
+			}
+		}
+	}
+	println!("No possible combination found");
+}
